44e97b7f4caa3dbae088bd8704c9474e0863db51
kaycee <Nwobodoe71@gmail.com>
2021-10-26 10:46:39 +0100

    Add user model functions

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,10 +1,8 @@
 let availableBooks = require("../data/books");
 const { v4: uuidV4 } = require("uuid");
-let users = require('../data/users.json')
+let users = require("../data/users.json");
 
 const { writeDataToFile } = require("../utils");
-const { resolve } = require("path/posix");
-const { rejects } = require("assert");
 
 function findAll() {
   return new Promise((resolve, reject) => {
@@ -19,13 +17,12 @@ function findById(id) {
   });
 }
 
-
-function create(book){
-    return new Promise((resolve, reject)=>{
-        const newBook = {id:uuidV4(), ...books}
-        availableBooks.push(newBook)
-        writeDataToFile('../data/books.json', availableBooks)
-    })
+function create(book) {
+  return new Promise((resolve, reject) => {
+    const newBook = { id: uuidV4(), ...book };
+    availableBooks.push(newBook);
+    writeDataToFile("../data/books.json", availableBooks);
+  });
 }
 
 function update(id, book) {
@@ -35,4 +32,21 @@ function update(id, book) {
     writeDataToFile("../data/books.json", availableBooks);
     resolve(availableBooks[index]);
   });
+}
+
+function remove(id) {
+  return new Promise((resolve, reject) => {
+    availableBooks = availableBooks.filter((p) => p.id !== id);
+    writeDataToFile("./data/availableBooks.json", availableBooks);
+    resolve();
+  });
+}
+
+
+module.exports = {
+    findAll,
+    findById,
+    create,
+    update,
+    remove
 }
--- models/userModels.js
+++ models/userModels.js
@@ -1,1 +1,48 @@
-[]
+const { v4: uuidV4 } = require("uuid");
+let users = require("../data/users.json");
+
+function findAllUsers(){
+    return new Promise((resolve, reject) => {
+        resolve(users);
+      });
+}
+
+function findUserById(id) {
+    return new Promise((resolve, reject) => {
+      const user = users.filter((u) => u.id === id);
+      resolve(user)
+    })
+}
+
+function createUser(user) {
+    return new Promise((resolve, reject) => {
+      const newUser = { id: uuidV4(), ...user };
+      users.push(newUser);
+      writeDataToFile("../data/users.json", users);
+    });
+  }
+
+  
+function updateUser(id,user) {
+    return new Promise((resolve, reject) => {
+        const index = users.findIndex((u) => u.id === id);
+        users[index] = { id, ...user };
+        writeDataToFile("../data/users.json", users);
+        resolve(users[index]);
+      });
+  }
+
+  function removeUser(id) {
+    return new Promise((resolve, reject) => {
+      users = users.filter((p) => p.id !== id);
+      writeDataToFile("./data/users.json", users);
+      resolve();
+    });
+
+  module.exports={
+      findAllUsers,
+      findUserById,
+      createUser,
+      updateUser,
+
+  }
